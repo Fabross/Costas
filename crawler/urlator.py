5f66b17d8c713d3802240b08fa754bbada5001cf
Fabricio <fabricio.rosales.12@sansano.usm.cl>
2019-01-16 14:38:52 -0300

    branch cleaned for only documentation

--- crawler/urlator.py
+++
@@ -1,8 +0,0 @@
-ids = open("newskypatch.txt","r" )
-urls = open("skypatchdata.txt","w+")
-for l in ids:
-	line = l.strip("\n")
-	urls.write("http://www.astrouw.edu.pl/cgi-asas/asas_cgi_get_data?{},asas3\n".format(line))
-	
-ids.close()
-urls.close()
